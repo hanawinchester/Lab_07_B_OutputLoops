01ed6a00d47d24fb635b529d7e3d188f28201f5e
hanawinchester <hana.rose.winchester@gmail.com>
2022-07-03 11:53:06 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main
+{
+    static String rowOne = "";
+    public static void main(String[] args)
+    {
+        for (int col=0; col<=4; col++)
+        {
+            rowOne = rowOne + "*";
+            System.out.println(rowOne);
+        }
+
+        System.out.println("\r\n");
+
+        //Reversed
+        for (int col=4; col>=0; col--)
+        {
+            for (int repeat=0; repeat<=col; repeat++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\r\n");
+
+        //Block
+        for (int col=0; col<=4; col++)
+        {
+            System.out.println("*****");
+        }
+    }
+
+}
